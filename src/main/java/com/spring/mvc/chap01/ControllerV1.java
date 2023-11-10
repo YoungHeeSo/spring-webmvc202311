d32dd33445d8a9524bbdc1f50498fa1116306ec7
YoungHeeSo <apdlfdldjqtm@gmail.com>
2023-11-10 11:32:57 +0900

    # 여러가지 요청 처리

--- src/main/java/com/spring/mvc/chap01/ControllerV1.java
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -2,8 +2,7 @@ package com.spring.mvc.chap01;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,7 +27,7 @@ public class ControllerV1 {
     }
 
 //    요청 파라미터 읽기
-//    1. HttpServletRequset 객체 이용하기
+//    1. HttpServletRequest 객체 이용하기
 //    ex ) /person?name=kim&age=30
     @RequestMapping("/person")
     public String person(HttpServletRequest request){
@@ -40,7 +39,7 @@ public class ControllerV1 {
         return "";
     }
 
-//    2. @RequestRaram 사용하기
+//    2. @RequestParam 사용하기
 //    ex ) major?stu=park&major=business&grade=3
     @RequestMapping("/major")
     public String major(String stu,
@@ -51,4 +50,38 @@ public class ControllerV1 {
         System.out.println("grade = " + grade);
         return "";
     }
+
+//    3. DTO(Data Tranfer Object) 객체 사용하기
+//    -> 파라미터의 양이 엄청 많거나 서로 연관되어 있는 경우에 사용
+//    ex ) /order?orderNum=123&goodsName=구두&amount=3&price=200000
+    @RequestMapping("/order")
+    public String order(OrderRequestDTO dto){
+        System.out.println("dto = " + dto);
+
+        System.out.println(dto.getGoodsName());
+        return "";
+    }
+
+//    4. URI 경로에 붙어있는 데이터 읽기
+//    ex ) /member/kim/107
+    @RequestMapping("/member/{userName}/{userNo}")
+    public String member(
+            @PathVariable String userName,
+            @PathVariable int userNo){
+        System.out.println("userName = " + userName);
+        System.out.println("userNo = " + userNo);
+        return "";
+    }
+
+//    5. POST 요청 데이터 읽기
+//    -> food.jsp에서 보낸 데이터 읽기
+//    Post으로 요청하지 않으면 요청을 거절한다
+//    @RequestMapping(value = "/food-select", method = RequestMethod.POST)
+    @PostMapping("/food-select")
+    public String select(String foodName, String category){
+        System.out.println("foodName = " + foodName);
+        System.out.println("category = " + category);
+        return "index";
+    }
+
 }
