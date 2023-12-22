d0a434ccf2fc01ffb4901b78b37c4cf8a41e9ea3
YoungHeeSo <apdlfdldjqtm@gmail.com>
2023-12-22 11:27:49 +0900

    # 회원관리 서비스, 비밀번호 인코딩, 로그인 검증

---
+++ src/main/java/com/spring/mvc/chap05/dto/request/LoginRequestDTO.java
@@ -0,0 +1,17 @@
+package com.spring.mvc.chap05.dto.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class LoginRequestDTO {
+
+    private String account;
+    private String password;
+    private boolean autoLogin; // 자동 로그인 체크 여부
+}
---
+++ src/main/java/com/spring/mvc/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.spring.mvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 시큐리티 기본 설정 (권한처리, 초기로그인화면 없애기 ...)
+    @Bean // 라이브러리 클래스 같은, 내가 만들지 않은, 남의 객체를 등록해서 주입받기 위한 아노테이션
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.csrf().disable() // CSRF-토근공격을 방지하기 위한 장치 해제
+                // 모든 요청에 대해 인증을 하지 않겠다
+                .authorizeHttpRequests()
+                .antMatchers("/**")
+                .permitAll();
+
+        return http.build();
+    }
+
+    // 비밀번호 암호화 객체를 빈 등록
+    @Bean
+    public PasswordEncoder encoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
