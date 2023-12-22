961f17ac6a16d754eefc16f22e5770b5bc19c2f9
YoungHeeSo <apdlfdldjqtm@gmail.com>
2023-12-22 09:46:11 +0900

    # 회원 관리 entity, mapper 생성

---
+++ src/test/java/com/spring/mvc/chap05/repository/MemberMapperTest.java
@@ -0,0 +1,12 @@
+package com.spring.mvc.chap05.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberMapperTest {
+
+    MemberMapper memberMapper;
+
+}
